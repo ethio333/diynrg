afb8476695db75c99569114240f471400fa03d54
Michael Briden <bridenmj@uw.edu>
2018-01-30 16:26:16 -0800

    Test Class Created by Michael Briden

--- diynrg/src/gui/TestClassMichael.java
+++ diynrg/src/gui/TestClassMichael.java
@@ -4,7 +4,7 @@ public class TestClassMichael {
 
 	public static void main(String[] args) {
 		for(int i=1; i<4;i++) {
-			System.out.println("Hello World");
+			System.out.println("Hello World!!!");
 		}
 
 	}
