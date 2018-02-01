123279c6b98a496e7de96900dc382789696dd9ff
Kevin <Kevin@10.0.0.153>
2018-02-01 12:56:57 -0800

    Edit by Kevin

--- diynrg/src/gui/TestClassMichael.java
+++ diynrg/src/gui/TestClassMichael.java
@@ -3,8 +3,14 @@ package gui;
 public class TestClassMichael {
 
 	public static void main(String[] args) {
-		for(int i=1; i<4;i++) {
+		int i;
+		for(i=1; i<4;i++) {
 			System.out.println("Hello World!!!");
+		}
+		
+		String[] kevin = {"Hello", "it", "is", "kevin"};
+		for (i = 0; i < 4; i++) {
+			System.out.print(kevin[i] + " ");
 		}
 
 	}
