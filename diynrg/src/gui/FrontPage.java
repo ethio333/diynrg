e20dc376ff5a557e61c991d6e3b3f9c7c0611182
FKMD Project <35471960+FKMD@users.noreply.github.com>
2018-02-20 21:57:20 -0800

    Update FrontPage.java

--- diynrg/src/gui/FrontPage.java
+++ diynrg/src/gui/FrontPage.java
@@ -1,3 +1,18 @@
+
+/*
+ * diynrgMain.java
+ *
+ * Developers: 
+ * Denis Yakovlev, 
+ * Baisalbek Urustanbekov, 
+ * Michael J. Briden, 
+ * Fasil Ayenew, 
+ * Kevin Nguyen
+ * 
+ * DIY project for energy saving.
+ * GUI class. 
+ */
+
 package gui;
 
 
