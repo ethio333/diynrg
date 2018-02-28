4f61b9dc3196ac18ad60683b5a8fcf1ce7d0b258
Michael Briden <bridenmj@uw.edu>
2018-02-28 00:38:00 -0800

    About class and version file.

--- diynrg/src/backend/About.java
+++ diynrg/src/backend/About.java
@@ -4,30 +4,38 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * A wrapper class containing the version and developer information
+ * for fkmd
+ * @author mike briden
+ * 2/27/2018
+ */
 public class About {
-	String version;
-	String date;
-	String name1;
-	String name2;
-	String name3;
-	String name4;
-	String website;
-	
+	private String version; // current version of diynrg
+	private String date; // date of last update
+	private String name1; // dev 1
+	private String name2; // dev 2
+	private String name3; // dev 3
+	private String name4; // dev 4
+	private String website; // website
 	/**
-	Read the given file into the TA
+	Read the given file into data.
 	*/
-	void About() {
+	public About() {
+		
 		File inFile = new File("version.txt") ;
+		
 		try {
 			Scanner in = new Scanner( inFile ) ;
 			while (in.hasNext() ) {
-				version = in.nextLine() ;
-				date = in.nextLine() ;
-				name1 = in.nextLine() ;
-				name2 = in.nextLine() ;
-				name3 = in.nextLine() ;
-				name4 = in.nextLine() ;
-				website = in.nextLine() ;
+				version = in.nextLine();
+				date= in.nextLine();
+				name1= in.nextLine();
+				name2= in.nextLine();
+				name3= in.nextLine();
+				name4= in.nextLine();
+				website= in.nextLine();
+				
 			}
 			in.close();
 		} catch (IOException io ) {
@@ -36,46 +44,59 @@ public class About {
 	}
 	
 	/**
-	 * shows the version
+	 * shows the current version
+	 * @return version
 	 */
 	String displayVersion(){
 		return version;
 	}
 	
+	/**
+	 * shows the date of last update
+	 * @return date
+	 */
 	String displayDate() {
 		return date;
 	}
 	
+	/**
+	 * display name of dev1
+	 * @return name1
+	 */
 	String displayName1(){
 		return name1;
 	}
 	
+	/**
+	 * name of dev2
+	 * @return name2
+	 */
 	String displayName2(){
 		return name2;
 	}
 	
+	/**
+	 * name of dev3
+	 * @return name3
+	 */
 	String displayName3(){
 		return name3;
 	}
 	
+	/**
+	 * name of dev4
+	 * @return name4
+	 */
 	String displayName4(){
 		return name4;
 	}
 	
+	/**
+	 * Display website
+	 * @return website
+	 */
 	String displayWebsite() {
 		return website;
-	}
-	
-	public static void main(String[] args) {
-		About abt = new About();
-		System.out.println(abt.version);
-		System.out.println(abt.date);
-		System.out.println(abt.name1);
-		System.out.println(abt.name2);
-		System.out.println(abt.name3);
-		System.out.println(abt.name4);
-		System.out.println(abt.website);
-
 	}
 	
 	
