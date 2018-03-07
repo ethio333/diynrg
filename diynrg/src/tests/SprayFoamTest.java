645dce73f8f294eb2825a1cf5f018b254dc3023d
Kevin Nguyen <Kevin.nguyen206@gmail.com>
2018-03-06 18:47:46 -0800

    Windows tests

---
+++ diynrg/src/tests/SprayFoamTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import backend.SprayFoam;
+
+/**
+ * @author Kevin
+ *
+ */
+public class SprayFoamTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link backend.SprayFoam#SprayFoam(int, int)}.
+	 */
+	@Test
+	public final void testSprayFoam() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link backend.SprayFoam#getWorstedEnergyConsumption()}.
+	 */
+	@Test
+	public final void testGetWorstedEnergyConsumption() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link backend.SprayFoam#getEnergyConsumptionForQuantity()}.
+	 */
+	@Test
+	public final void testGetEnergyConsumptionForQuantity() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link backend.SprayFoam#getBaseEnergyConsumptionForQuantity()}.
+	 */
+	@Test
+	public final void testGetBaseEnergyConsumptionForQuantity() {
+		fail("Not yet implemented");
+	}
+
+}
